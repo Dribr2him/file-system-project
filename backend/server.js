e1546bb476b22328948b02df7b1f5176dec90349
Dribr2him <gkh8nh99w7@privaterelay.appleid.com>
2026-04-29 14:42:09 +0300

    fix wildcard route

--- backend/server.js
+++ backend/server.js
@@ -206,7 +206,7 @@ app.use("/uploads", express.static("uploads"));
 // ===== Serve React =====
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
